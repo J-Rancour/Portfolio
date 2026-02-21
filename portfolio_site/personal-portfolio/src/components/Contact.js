cdd659e13014eede814a29e1b63e5aeaeca29f98
J-Ran <jacobrancour1189@gmail.com>
2026-02-21 00:36:47 -0500

    Finished the project.js file and ended up just copying the App.css file from the tutorial. Due to the video not highlighting all the text in the walkthrough I would rather have it all at least and reflect on it later.

---
+++ portfolio_site/personal-portfolio/src/components/Contact.js
@@ -0,0 +1,17 @@
+export const Contact = () => {
+    const formInitialDetails = {
+        firstName: '',
+        lastName: '',
+        email: '',
+        phone: '',
+        message: '',
+    }
+    const [formDetails, setFormDetails] = useState(formInitialDetails);
+    const [buttonText, setButton] = useState('Send');
+    const [status, setStatus] = useStage({});
+
+    
+    return (
+
+    )
+}
